0a1b7cb2ae7b348f6db34149e45fcabcde91d05b
Lucas <luksmrbusiness@gmail.com>
2026-01-27 15:04:27 -0300

    chore: added basic dart project

---
+++ test/robots_mars_challenge_test.dart
@@ -0,0 +1,8 @@
+import 'package:robots_mars_challenge/robots_mars_challenge.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
